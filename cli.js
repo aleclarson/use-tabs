ad0a8f11aa46b0a8b62e6b5a65193582df889221
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-27 11:48:22 -0400

    chore: run use-tabs on itself

--- cli.js
+++ cli.js
@@ -3,32 +3,32 @@ const sade = require('sade')
 const { green } = require('kleur')
 
 sade('use-tabs', true)
-  .version(require('./package.json').version)
-  .option('-n, --check', 'Skip file writing')
-  .action(async opts => {
-    const fs = require('fs')
-    const path = require('path')
-    const useTabs = require('.')
+	.version(require('./package.json').version)
+	.option('-n, --check', 'Skip file writing')
+	.action(async opts => {
+		const fs = require('fs')
+		const path = require('path')
+		const useTabs = require('.')
 
-    let count = 0
-    const cwd = process.cwd()
-    const files = await useTabs(cwd, opts._, (file, code) => {
-      if (!opts.n) fs.writeFileSync(file, code)
-      console.log(green('✓ ' + path.relative(cwd, file)))
-      count++
-    })
+		let count = 0
+		const cwd = process.cwd()
+		const files = await useTabs(cwd, opts._, (file, code) => {
+			if (!opts.n) fs.writeFileSync(file, code)
+			console.log(green('✓ ' + path.relative(cwd, file)))
+			count++
+		})
 
-    if (!files.length) {
-      console.log(`⚠️  No files were found.`)
-      return
-    }
+		if (!files.length) {
+			console.log(`⚠️  No files were found.`)
+			return
+		}
 
-    const message = !count
-      ? `Nothing changed`
-      : opts.n
-      ? `Found ${count} file${count > 1 ? 's' : ''} with spaces`
-      : `Changed ${count} file${count > 1 ? 's' : ''}`
+		const message = !count
+			? `Nothing changed`
+			: opts.n
+			? `Found ${count} file${count > 1 ? 's' : ''} with spaces`
+			: `Changed ${count} file${count > 1 ? 's' : ''}`
 
-    console.log(`✨  ${message}.`)
-  })
-  .parse(process.argv)
+		console.log(`✨  ${message}.`)
+	})
+	.parse(process.argv)
--- index.js
+++ index.js
@@ -8,39 +8,39 @@ const { crawl } = require('recrawl')
 const ALWAYS_SKIP = ['node_modules', '.git']
 
 /**
- * Find all files in the given root directory which match the given globs.
- * For each file, replace all space indentation with tabs and pass each result
- * to the given callback.
- */
+	* Find all files in the given root directory which match the given globs.
+	* For each file, replace all space indentation with tabs and pass each result
+	* to the given callback.
+	*/
 async function useTabs(root, globs, each) {
-  const skip = globs.filter(glob => glob[0] == '!')
-  const files = await crawl(root, {
-    only: globs.filter(glob => !skip.includes(glob)),
-    skip: ALWAYS_SKIP.concat(skip.map(glob => glob.slice(1))),
-  })
-  files.forEach(name => {
-    const file = path.join(root, name)
-    const code = fs.readFileSync(file, 'utf8')
-    const { indent } = detect(code)
-    if (indent && indent !== '\t') {
-      each(file, convert(code, indent))
-    }
-  })
-  return files
+	const skip = globs.filter(glob => glob[0] == '!')
+	const files = await crawl(root, {
+		only: globs.filter(glob => !skip.includes(glob)),
+		skip: ALWAYS_SKIP.concat(skip.map(glob => glob.slice(1))),
+	})
+	files.forEach(name => {
+		const file = path.join(root, name)
+		const code = fs.readFileSync(file, 'utf8')
+		const { indent } = detect(code)
+		if (indent && indent !== '\t') {
+			each(file, convert(code, indent))
+		}
+	})
+	return files
 }
 
 module.exports = useTabs
 
 function convert(code, indent) {
-  const indentRegex = /^([\u0020\t]*)/
-  return code
-    .split(/\r?\n/g)
-    .map((line, i) => {
-      const { length } = line.match(indentRegex)[0]
-      return length
-        ? '\t'.repeat(Math.round(length / indent.length)) +
-            line.replace(/^\s+/, '')
-        : line
-    })
-    .join(os.EOL)
+	const indentRegex = /^([\u0020\t]*)/
+	return code
+		.split(/\r?\n/g)
+		.map((line, i) => {
+			const { length } = line.match(indentRegex)[0]
+			return length
+				? '\t'.repeat(Math.round(length / indent.length)) +
+						line.replace(/^\s+/, '')
+				: line
+		})
+		.join(os.EOL)
 }
